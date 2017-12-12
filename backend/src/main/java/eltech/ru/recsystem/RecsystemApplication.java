fbc8bdc3f2b65e663e7f6c3ddcfde4b1e1b85acd
Kirill Uliashev <qwertypalmO@gmail.com>
2017-12-13 00:55:39 +0300

    initial commit

---
+++ backend/src/main/java/eltech/ru/recsystem/RecsystemApplication.java
@@ -0,0 +1,12 @@
+package eltech.ru.recsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecsystemApplication.class, args);
+	}
+}
